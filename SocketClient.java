00b86723a0e5732b92351c8b3f3f90e39f71372c
Sophie Tao <121151894+sophietao127@users.noreply.github.com>
2023-04-10 23:37:13 -0700

    Add files via upload

---
+++ SocketClient.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+
+public class SocketClient {
+  public static void main(String[] args) throws Exception {
+    String host = args[0];
+    int port = Integer.valueOf(args[1]);
+
+    try {
+			Socket sock = new Socket(host, port);
+			InputStream in = sock.getInputStream();
+			int readChar = 0;
+
+			while ((readChar = in.read()) != -1) {
+				System.out.write(readChar);
+			}
+			sock.close();
+		} catch(IOException ex) {
+			System.err.println(ex);
+		}
+  }
+}
