91dded8f2441cf8bc4aa9f36cfafb0cb4e9f921e
Sophie Tao <121151894+sophietao127@users.noreply.github.com>
2023-04-10 23:57:10 -0700

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -1,22 +1,26 @@
 import java.net.*;
 import java.io.*;
+import java.util.logging.Logger;
 
 public class SocketClient {
+  private static Logger logger = Logger.getLogger(SocketClient.class.getName());
   public static void main(String[] args) throws Exception {
     String host = args[0];
     int port = Integer.valueOf(args[1]);
+    // Log a info level msg
+    logger.info("This is level info logging");
 
     try {
-    	Socket sock = new Socket(host, port);
-	InputStream in = sock.getInputStream();
-	int readChar = 0;
+      Socket sock = new Socket(host, port);
+      InputStream in = sock.getInputStream();
+      int readChar = 0;
 
-	while ((readChar = in.read()) != -1) {
-		System.out.write(readChar);
-	}
-	sock.close();
-	} catch(IOException ex) {
-		System.err.println(ex);
-	}
+      while ((readChar = in.read()) != -1) {
+	  System.out.write(readChar);
+      }
+      sock.close();
+      } catch(IOException ex) {
+        System.err.println(ex);
+      }
   }
 }
